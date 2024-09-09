ac43cb44594dfb8f0bde108a2341b8950ac8a6bc
Chrisin25 <chrising2001@gmail.com>
2024-09-09 11:55:58 +0530

    -

--- Student_Management/main.py
+++ Student_Management/main.py
@@ -4,14 +4,14 @@ new_school=School()
 print("new school created")
 stop=False
 while not stop:
-    choice = int(input(print("Enter your choice:1.add student 2.remove student 3.search student 4.generate student.")))
+    choice = int(input("Enter your choice:1.add student 2.remove student 3.search student 4.generate student."))
     if choice==1:
         new_school.add_student()
     elif choice==2:
-        student_id = int(input(print("enter student id:")))
+        student_id = int(input("enter student id:"))
         new_school.remove_student(student_id)
     elif choice==3:
-        student_id=int(input(print("enter student id:")))
+        student_id=int(input("enter student id:"))
         new_school.search_student(student_id)
     elif choice==4:
         new_school.generate_student()
